555b2b69f499e5043900ab27d568a8bbd7e370b8
nicolas morales <menthuthuyoupi.nmp@gmail.com>
2023-11-19 22:11:30 -0300

    subiendo todo

---
+++ index.js
@@ -0,0 +1,11 @@
+precio = 400000
+cantidad = 0
+
+precioSpan = document.querySelector('.precio-inicial')
+precioSpan.innerHTML = precio
+
+total = precio*cantidad
+
+valorTotal = document.querySelector('.valor-total')
+valorTotal.innerHTML = total
+
